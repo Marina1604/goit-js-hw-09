12b70ffa04194a2c56719c1828d2a2823800cad8
Marina1604 <117771855+Marina1604@users.noreply.github.com>
2023-04-16 21:53:00 +0300

    S

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+startBtnEl.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtnEl.disabled = true;
+        stopBtnEl.disabled = false;
+    }, 1000)
+});
+
+stopBtnEl.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+});
